a98ce298d0072b0d8d68a35ba57298aac93d04b8
Benjamin Camacho <benjamin.camacho@dabarc.com>
2019-10-30 15:37:00 -0600

    Se elimina superfis de lib

--- gulpfile.js
+++ gulpfile.js
@@ -69,6 +69,9 @@ function modules() {
   // animate.css
   var animateCSS = gulp.src('./node_modules/animate.css/*.css')
     .pipe(gulp.dest('./vendor/animate'));
+  // superfish
+  var animateCSS = gulp.src('./node_modules/superfish/dist/js/*')
+    .pipe(gulp.dest('./vendor/superfish'));
   // jQuery
   var jquery = gulp.src([
       './node_modules/jquery/dist/*',
@@ -78,7 +81,7 @@ function modules() {
   // Venobox
   var venobox = gulp.src('./node_modules/venobox/venobox/*')
     .pipe(gulp.dest('./vendor/venobox'));
-  return merge(animateCSS,bootstrapJS, fontAwesomeCSS, fontAwesomeWebfonts, jquery, jqueryEasing, venobox, jqueryMigrate);
+  return merge(animateCSS, bootstrapJS, fontAwesomeCSS, fontAwesomeWebfonts, jquery, jqueryEasing, venobox, jqueryMigrate);
 }
 
 // CSS task
@@ -137,58 +140,54 @@ gulp.task('images', function () {
   return gulp
     .src('img/*.png')
     .pipe(
-      $.responsive(
-        {
-          '*.png': [
-            {
-              width: 300,
-              rename: {
-                suffix: '-300px',
-                extname: '.jpg'
-              },
-              format: 'jpeg'
+      $.responsive({
+        '*.png': [{
+            width: 300,
+            rename: {
+              suffix: '-300px',
+              extname: '.jpg'
             },
-            {
-              width: 600,
-              rename: {
-                suffix: '-600px',
-                extname: '.jpg'
-              }
-              // format option can be omitted because
-              // format of output image is detected from new filename
-              // format: 'jpeg'
-            },
-            {
-              width: 1900,
-              rename: {
-                suffix: '-1900px',
-                extname: '.jpg'
-              },
-              // Do not enlarge the output image if the input image are already less than the required dimensions.
-              withoutEnlargement: true
-            },
-            {
-              // Convert images to the webp format
-              width: 630,
-              rename: {
-                suffix: '-630px',
-                extname: '.webp'
-              }
+            format: 'jpeg'
+          },
+          {
+            width: 600,
+            rename: {
+              suffix: '-600px',
+              extname: '.jpg'
             }
-          ]
-        },
-        {
-          // Global configuration for all images
-          // The output quality for JPEG, WebP and TIFF output formats
-          quality: 80,
-          // Use progressive (interlace) scan for JPEG and PNG output
-          progressive: true,
-          // Strip all metadata
-          withMetadata: false,
-          // Do not emit the error when image is enlarged.
-          errorOnEnlargement: false
-        }
-      )
+            // format option can be omitted because
+            // format of output image is detected from new filename
+            // format: 'jpeg'
+          },
+          {
+            width: 1900,
+            rename: {
+              suffix: '-1900px',
+              extname: '.jpg'
+            },
+            // Do not enlarge the output image if the input image are already less than the required dimensions.
+            withoutEnlargement: true
+          },
+          {
+            // Convert images to the webp format
+            width: 630,
+            rename: {
+              suffix: '-630px',
+              extname: '.webp'
+            }
+          }
+        ]
+      }, {
+        // Global configuration for all images
+        // The output quality for JPEG, WebP and TIFF output formats
+        quality: 80,
+        // Use progressive (interlace) scan for JPEG and PNG output
+        progressive: true,
+        // Strip all metadata
+        withMetadata: false,
+        // Do not emit the error when image is enlarged.
+        errorOnEnlargement: false
+      })
     )
     .pipe(gulp.dest('dist'))
 })

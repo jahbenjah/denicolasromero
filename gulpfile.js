cab51b4022c8461f4e6a0f7d4daec170fe9ea0ae
Benjamin Camacho <benjamin.camacho@dabarc.com>
2019-10-30 12:06:48 -0600

    Se agregn owl , superfish, venovox animate jquery plugin

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,8 @@ const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
+const responsive = require('gulp-responsive')
+var $ = require('gulp-load-plugins')()
 
 // Load package.json for banner
 const pkg = require('./package.json');
@@ -126,6 +128,67 @@ function watchFiles() {
   gulp.watch(["./js/**/*", "!./js/**/*.min.js"], js);
   gulp.watch("./**/*.html", browserSyncReload);
 }
+
+//Image responsive 
+gulp.task('images', function () {
+  return gulp
+    .src('img/*.png')
+    .pipe(
+      $.responsive(
+        {
+          '*.png': [
+            {
+              width: 300,
+              rename: {
+                suffix: '-300px',
+                extname: '.jpg'
+              },
+              format: 'jpeg'
+            },
+            {
+              width: 600,
+              rename: {
+                suffix: '-600px',
+                extname: '.jpg'
+              }
+              // format option can be omitted because
+              // format of output image is detected from new filename
+              // format: 'jpeg'
+            },
+            {
+              width: 1900,
+              rename: {
+                suffix: '-1900px',
+                extname: '.jpg'
+              },
+              // Do not enlarge the output image if the input image are already less than the required dimensions.
+              withoutEnlargement: true
+            },
+            {
+              // Convert images to the webp format
+              width: 630,
+              rename: {
+                suffix: '-630px',
+                extname: '.webp'
+              }
+            }
+          ]
+        },
+        {
+          // Global configuration for all images
+          // The output quality for JPEG, WebP and TIFF output formats
+          quality: 80,
+          // Use progressive (interlace) scan for JPEG and PNG output
+          progressive: true,
+          // Strip all metadata
+          withMetadata: false,
+          // Do not emit the error when image is enlarged.
+          errorOnEnlargement: false
+        }
+      )
+    )
+    .pipe(gulp.dest('dist'))
+})
 
 // Define complex tasks
 const vendor = gulp.series(clean, modules);
